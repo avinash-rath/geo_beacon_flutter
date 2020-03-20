57fbf5d410aa636bf2ee895e1ed0c2abeddea3a8
Avinash Rath <avinash.rrath@gmail.com>
2020-03-20 18:13:57 +0530

    Implement broadcast location concept

--- server/server.js
+++ server/server.js
@@ -1,16 +1,21 @@
 var WebSocketServer = require('websocket').server;
 var http = require('http');
 
+//__________________________________________________
+
+//__________________________________________________
 var server = http.createServer(function(request, response) {
 });
 server.listen(1337, function() {
     console.log((new Date()) + " Server is listening on port : 1337");
  });
-
+//__________________________________________________
 // create the server
 wsServer = new WebSocketServer({
   httpServer: server
 });
+
+var location='';
 
 // WebSocket server
 wsServer.on('request', function(request) {
@@ -18,14 +23,14 @@ wsServer.on('request', function(request) {
       + request.origin + '.');
   var connection = request.accept(null, request.origin);
   console.log((new Date()) + ' Connection accepted.');
-  connection.sendUTF('Hello from the other side!')
+  connection.sendUTF('Message from server : Hello from the other side!')
 
-  // This is the most important callback for us, we'll handle
-  // all messages from users here.
+  // Handle messages
   connection.on('message', function(message) {
     if (message.type === 'utf8') {
       // process WebSocket message
-      connection.sendUTF(message);
+      location = message.utf8Data
+      connection.sendUTF('Message from server '+message.utf8Data);
     }
   });
 
@@ -34,3 +39,40 @@ wsServer.on('request', function(request) {
     console.log('Connection closed');
   });
 });
+//__________________________________________________
+var broadcastServer = http.createServer(function(request, response) {
+});
+
+broadcastServer.listen(1338, function () {
+  console.log((new Date())+" Broadcast server running on port : 1338");
+})
+
+broadcastWsServer = new WebSocketServer({
+  httpServer:broadcastServer
+});
+
+broadcastWsServer.on('request', function(request) {
+  console.log((new Date()) + ' Connection from origin '
+    + request.origin + '.');
+  var connection = request.accept(null, request.origin);
+  console.log((new Date()) + ' Connection accepted.');
+  connection.sendUTF('Broadcasting location : '+location);
+
+  setInterval(function () {
+    connection.sendUTF('Broadcasting location : '+location);
+  },1000)
+
+  // Handle messages
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+     // process WebSocket message
+     connection.sendUTF(message.utf8Data);
+    }
+  });
+
+  connection.on('close', function(connection) {
+    // close user connection
+    console.log('Connection closed');
+  });
+});
+//__________________________________________________
