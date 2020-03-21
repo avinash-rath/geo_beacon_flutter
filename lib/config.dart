21c9c25a092db2d620f300350e168c0590461ba3
Avinash Rath <avinash.rrath@gmail.com>
2020-03-21 17:31:59 +0530

    Add activeLocListenerPort

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,5 @@
 String websocketServerUrl = 'ws://192.168.0.104';
 String locationSendPort = '1337';
 String broadcastListenerPort = '1338';
+String activeLocListenerPort = '1339';
 String httpServerUrl = 'http://192.168.0.104';
