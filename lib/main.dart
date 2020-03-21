505bdf2fb3b01c88c155b9142990bdcd479a6c26
Avinash Rath <avinash.rrath@gmail.com>
2020-03-21 17:32:57 +0530

    Core structure ready

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:geo_beacon_flutter/activeLocListener.dart';
 import 'package:geo_beacon_flutter/broadcastListener.dart';
 import 'package:geo_beacon_flutter/config.dart';
+import 'package:geo_beacon_flutter/shareLocation.dart';
 import 'package:web_socket_channel/io.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Beacon',
-      home: HomePage(
-        channel: IOWebSocketChannel.connect('$websocketServerUrl:$locationSendPort'),
-      ),
+      home: HomePage(),
     );
   }
 }
 
 class HomePage extends StatefulWidget {
-  final WebSocketChannel channel;
-
-  HomePage({@required this.channel});
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController _controller = new TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Header'),
+        title: Text('Beacon'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Form(
-              child: TextFormField(
-                controller: _controller,
-                decoration: InputDecoration(labelText: 'Send a message'),
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 3,
+            child: ShareLocation(
+              channel: IOWebSocketChannel.connect(
+                '$websocketServerUrl:$locationSendPort',
               ),
             ),
-            StreamBuilder(
-              stream: widget.channel.stream,
-              builder: (context, snapshot) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 24.0),
-                  child: Text(snapshot.hasData ? '${snapshot.data}' : ''),
-                );
-              },
-            ),
-            SizedBox(height: 50.0),
-            BroadcastListener(
-              channel: IOWebSocketChannel.connect(
-                  '$websocketServerUrl:$broadcastListenerPort'),
-            ),
-          ],
-        ),
+          ),
+          Expanded(child:  ActiveLocListener(
+        channel: IOWebSocketChannel.connect(
+            '$websocketServerUrl:$activeLocListenerPort'),
+      ),)
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _sendMessage,
-        tooltip: 'Send message',
-        child: Icon(Icons.send),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }
-
-  void _sendMessage() {
-    if (_controller.text.isNotEmpty) {
-      widget.channel.sink.add(_controller.text);
-    }
-  }
-
-  @override
-  void dispose() {
-    widget.channel.sink.close();
-    super.dispose();
   }
 }
